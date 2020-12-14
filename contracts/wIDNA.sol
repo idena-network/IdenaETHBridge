c55bef886063c7f474a7963c3415ef75758f2c62
RealMahmoud <RealMahmoudm@gmail.com>
2020-12-15 00:07:43 +0200

    rename to WrappedIdena

--- contracts/wIDNA.sol
+++ contracts/wIDNA.sol
@@ -8,7 +8,7 @@ import "./token/ERC20/ERC20.sol";
 import "./token/ERC20/ERC20Burnable.sol";
 
 
-contract ERC20PresetMinter is Context, AccessControl, ERC20Burnable {
+contract WrappedIdena is Context, AccessControl, ERC20Burnable {
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
 
     constructor(string memory name, string memory symbol) public ERC20(name, symbol) {
@@ -16,7 +16,7 @@ contract ERC20PresetMinter is Context, AccessControl, ERC20Burnable {
         _setupRole(MINTER_ROLE, _msgSender());
     }
     function mint(address to, uint256 amount) public virtual {
-        require(hasRole(MINTER_ROLE, _msgSender()), "ERC20PresetMinter: must have minter role to mint");
+        require(hasRole(MINTER_ROLE, _msgSender()), "WrappedIdena: must have minter role to mint");
         _mint(to, amount);
     }
     function _beforeTokenTransfer(address from, address to, uint256 amount) internal virtual override(ERC20) {
